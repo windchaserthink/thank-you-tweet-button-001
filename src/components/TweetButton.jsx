d18609e27da81134f1a1c66ad0464b404dea8fc1
windchaserthink <windchaserthink@gmail.com>
2024-11-20 23:08:18 +0800

    build: specify items babel should  compile

--- src/components/TweetButton.jsx
+++ src/components/TweetButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import '../index.css';
 
 export function TweetButton () {
   const tweetURL = "https://twitter.com/intent/tweet?text=I%20just%20won%20a%20free%20ticket%20to%20the%20React%20Europe%20conference!%20%F0%9F%8E%89%20%23ReactEurope%20%F0%9F%8E%89%20%23ReactConf%20%F0";
